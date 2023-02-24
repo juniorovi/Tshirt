aec6f1d1155c193744d45b0d23fee261f183bde8
juniorovi <mdtarikulislam258000@gmail.com>
2023-02-24 19:31:28 +0600

    createContext() API to avoid Prop drilling

--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -1,27 +1,32 @@
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
 import Aunty from '../Aunty/Aunty';
 import Father from '../Father/Father'
 import Uncle from '../Uncle/Uncle';
 import './Grandpa.css'
 
+// export const RingContext = createContext('Dimond Ring');
+export const RingContext = createContext();
+
 const Grandpa = () => {
     const [house, setHouse] = useState(1);
     const gift = 'Dimond Ring'
-    const handleBuyHouse = ()=>{
+    const handleBuyHouse = () => {
         const newHouse = house + 1;
         setHouse(newHouse);
     }
     return (
-        <div className='grandpa'>
-            <h3>Grandpa</h3>
-            <p>House: {house}</p>
-            <button onClick={()=> handleBuyHouse()}>Buy Home</button>
-            <section style={{ display: 'flex' }}>
-                <Father house={house} gift={gift}></Father>
-                <Uncle house={house}></Uncle>
-                <Aunty house={house}></Aunty>
-            </section>
-        </div>
+        <RingContext.Provider value='Alur Ring'>
+            <div className='grandpa'>
+                <h3>Grandpa</h3>
+                <p>House: {house}</p>
+                <button onClick={() => handleBuyHouse()}>Buy Home</button>
+                <section style={{ display: 'flex' }}>
+                    <Father house={house} gift={gift}></Father>
+                    <Uncle house={house}></Uncle>
+                    <Aunty house={house}></Aunty>
+                </section>
+            </div>
+        </RingContext.Provider>
     );
 };
 
--- src/components/Special/Special.js
+++ src/components/Special/Special.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
 const Special = ({gift}) => {
+    const ring = useContext(RingContext)
     return (
         <div>
             <h4>Special</h4>
-            <p>Gift from grandpa: {gift}</p>
+            {/* <p>Gift from grandpa: {gift}</p> */}
+            <p>Gift from grandpa: {ring}</p>
         </div>
     );
 };
