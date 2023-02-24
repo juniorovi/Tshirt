a0d77f4c9ac6f922f560f8a5436553c18564f00c
juniorovi <mdtarikulislam258000@gmail.com>
2023-02-24 17:49:19 +0600

    Share same data from parent to children to children

---
+++ src/components/Myself/Myself.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Myself = ({house}) => {
+    return (
+        <div>
+            <h3>Me</h3>
+            <p>House: {house}</p>
+        </div>
+    );
+};
+
+export default Myself;
